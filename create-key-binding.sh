4920c0180146da982f85759817f0bc66a56ef504
Yash Goyal <ygoyal@wigwag.com>
2018-10-25 16:57:10 +0530

    Create shortcut cant be called with sudo, fixed that

--- create-key-binding.sh
+++ create-key-binding.sh
@@ -8,7 +8,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
 "['$KEY_PATH/custom0/']"
 
 echo "Installing key shortcut..."
-# Take a screenshot of the entire display
+
 $BEGINNING/custom0/ name "Access WigWag India Door"
-$BEGINNING/custom0/ command "/usr/bin/open-ww-door-lock"
+$BEGINNING/custom0/ command "/usr/bin/ww-open-door-lock"
 $BEGINNING/custom0/ binding "<Shift><Ctrl>G"
--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ];then
-    echo "Please run as root"
-    exit
-fi
+# if [ "$EUID" -ne 0 ];then
+#     echo "Please run as root"
+#     exit
+# fi
 
 bold=$(tput bold)
 red=`tput setaf 1`
@@ -18,20 +18,21 @@ if command -v node &>/dev/null; then
     output "Found node.."
     node --version
 else
-	error "You don't have NODE on your machine. Are you in 18th century! Install NODE, you **** Bye..."
+	error "You don't have NODE on your machine. Are you in 18th century! Install NODE, you **** BTW you are fired..."
 	exit
 fi
 
 PWD=`pwd`
-PROG="/usr/bin/open-ww-door-lock"
-touch $PROG
-chmod 755 $PROG
+PROG="/usr/bin/ww-open-door-lock"
+sudo touch $PROG
+sudo chmod 755 $PROG
 NODE_BIN="/usr/bin/node"
 if [ ! -e $NODE_BIN ]; then
 	NODE_BIN=/usr/local/bin/node
 fi
-echo "#!/bin/sh
-$NODE_BIN $PWD/doorController-client-host.js open_door > /tmp/doorlock.log &" > $PROG
+sudo sh -c "echo \"#!/bin/sh
+$NODE_BIN $PWD/doorController-client-host.js open_door > /tmp/doorlock.log &\" > $PROG"
+
 output "Door controller client successfully installed!"
 npm install
 
